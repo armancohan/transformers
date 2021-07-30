4de7eaeb9bd901dd7b319cb38bf72ccbf3e5b4a5
Arman Cohan <armanc@s2-server5>
2021-07-29 17:36:31 -0700

    test

--- tests/test_modeling_t5_tf_conversion.py
+++ tests/test_modeling_t5_tf_conversion.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 
 
-def test_conversion(checkpoint1, checkpoint2):
+def _test_conversion(checkpoint1, checkpoint2):
     init_vars1 = dict(tf.train.list_variables(checkpoint1))
     init_vars2 = dict(tf.train.list_variables(checkpoint2))
 
@@ -42,21 +42,17 @@ def test_conversion(checkpoint1, checkpoint2):
                 continue  # already checked above
             if not np.allclose(v.astype(np.float32), weights2[k].astype(np.float32)):
                 assert False
-        return true
+        return True
 
     assert check_weights(weights1, weights2)
     assert check_weights(weights2, weights1)
 
 
-def main():
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint1", help="")
     parser.add_argument("--checkpoint2", help="")
     args = parser.parse_args()
-    test_conversion(args.checkpoint1, args.checkpoint2)
-
-
-if __name__ == "__main__":
-    main()
+    _test_conversion(args.checkpoint1, args.checkpoint2)
